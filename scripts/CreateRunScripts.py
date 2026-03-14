47816c7a54d31a171a6e9e29f4485c838fcc1847
Kir-Dor <k.doroshkov@g.nsu.ru>
2026-03-14 11:08:33 +0700

    fix scripts

--- scripts/CreateRunScripts.py
+++ scripts/CreateRunScripts.py
@@ -24,11 +24,8 @@ def create_shell_script(
 #PBS -l walltime=00:05:00
 #PBS -l select={nodes}:ncpus=8:mpiprocs={processes_per_node}
 #PBS -m n
-#PBS -o ~/lab1/output/{script_path.name}.log;
-#PBS -e ~/lab1/error/{script_path.name}.log;
-cd $PBS_O_WORKDIR
 mkdir -p ~/lab1/output
-mkdir -p ~/lab1/error
+cd ~/lab1/output
 MPI_NP=$(wc -l $PBS_NODEFILE | awk '{{ print $1 }}')
 echo “Number of MPI process: $MPI_NP“
 echo 'File $PBS_NODEFILE:'
